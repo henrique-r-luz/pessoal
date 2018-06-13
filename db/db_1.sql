73b07543f1ddab747270d31c50a02e3b9253775d
henrique <henrique@henrique-H87M-D3H>
2018-06-13 00:17:42 -0300

    cria banco

---
+++ db/db_1.sql
@@ -0,0 +1,29 @@
+create table categorias(
+ id serial primary key,
+ nome text
+
+);
+
+create table atualizacao(
+  id serial primary key,
+  data date,
+  valor_total real
+);
+
+
+create table titulos(
+	id serial primary key,
+	ativo text,
+	emissor text,
+	quantidade real,
+	tributos real,
+	valor_compra real,
+	valor_venda real,
+        atualizacao_id int  REFERENCES atualizacao ,
+        categoria_id int  REFERENCES categorias
+        
+      
+);
+
+
+
