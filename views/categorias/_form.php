8d7013d60fbe801049b42c633bee31baa0f2ee59
Henrique Luz <henrique_r_luz@yahoo.com.br>
2018-06-22 15:13:31 -0300

    css grid form

--- views/categorias/_form.php
+++ views/categorias/_form.php
@@ -11,18 +11,19 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
      
-     
-    <div>
-		<div class="col-xs-12 col-lg-12 no-padding">
-    <?= $form->field($model, 'id')->input([]) ?> 
+     <div class="row">
+    <div class="col-xs-6 col-sm-2 col-lg-2">
+    
+    <?= $form->field($model, 'id')->textInput() ?> 
     </div>
-		<div class="col-xs-12 col-lg-12 no-padding">
-        <?= $form->field($model, 'nome')->input([]) ?>
-     </div>
-		<div class="form-group">
+    <div class="col-lg-10 col-md-10">
+  
+        <?= $form->field($model, 'nome')->textInput() ?>
+    </div>
+   </div>  
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-    </div>
-		<div>
+
+		
     <?php ActiveForm::end(); ?>
 
 </div>
